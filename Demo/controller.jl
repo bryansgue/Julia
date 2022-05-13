dc0488dc23fb153c7cd1131948bd69954a306b19
bryansgue <bryansgue@gmail.com>
2022-05-13 20:01:42 -0300

    Hola

--- Demo/controller.jl
+++ Demo/controller.jl
@@ -9,14 +9,16 @@ using .geometry_msgs.msg
 maxloops = 1000
 rosloops = 0
 
+
 function hpd_callback(msg::Twist) 
+    global hdp
     hpd =[msg.linear.x, msg.linear.y, msg.linear.z,msg.angular.z]
-    println(hpd)
+    #println(hpd)
 end
 
 function sensor1_callback(msg::Point) 
     sensor1 =[msg.x, msg.y, msg.z]
-    println(sensor1)
+    #println(sensor1)
 end
 
 
@@ -34,22 +36,55 @@ end
 function main()
     
     a=rand()
-    
     t=10 #Segundos 
-    hz = 30 # Frecuencia de actualizacion
+    hz = 10 # Frecuencia de actualizacion
+    samples = t*hz
     loop_rate = Rate(hz)
 
+    psi = Array{Float64}(undef, samples)
+    vector = []
+    h = zeros(4, samples)
+    J = zeros(4,4)
+    hpd = zeros(4,1)
     for k in 1:(t*hz) 
         tic = time()
+        #push!(vector, k)
+        #psi[k]=rand()
+        h[:,k] = [0,0,0,rand()]
+        psi = h[4,k]
+        hxp = hpd[1]
+        # Ley de Control
+        J11 = cos(psi);
+        J12 = -sin(psi);
+        J13 = 0;
+        J14 = 0;
+
+        J21 = sin(psi);
+        J22 = cos(psi);
+        J23 = 0;
+        J24 = 0;
+
+        J31 = 0;
+        J32 = 0;
+        J33 = 1;
+        J34 = 0;
+
+        J41 = 0;
+        J42 = 0;
+        J43 = 0;
+        J44 = 1;
+
+        J = [J11 J12 J13 J14;J21 J22 J23 J24;J31 J32 J33 J34;J41 J42 J43 J44]
+
         
         sendvalues(pub,k)
         
-        rossleep(loop_rate)
-        toc = time()
+        rossleep(loop_rate); toc = time()
         dt = toc-tic
-        #println(1/dt)
+        #println(k , ": " ,psi[k] ," ", vector[k])
+        println(hxp)
     end
-    
+    #println(J)
 end
 
 
